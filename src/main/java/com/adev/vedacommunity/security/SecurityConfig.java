24acb9e13dca1db1dfe44201a95f2b9f79aec96a
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-08 21:15:29 +0900

    Todo: 로그인 관련 설정 필요

--- src/main/java/com/adev/vedacommunity/security/SecurityConfig.java
+++ src/main/java/com/adev/vedacommunity/security/SecurityConfig.java
@@ -9,9 +9,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -24,24 +24,24 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
         http
-
                 .csrf(CsrfConfigurer::disable)
-                .headers((headerConfig) ->
+                .formLogin(FormLoginConfigurer::disable)
+                .headers(headerConfig ->
                         headerConfig.frameOptions(frameOptionsConfig ->
-                                frameOptionsConfig.disable()
-                        )
+                                frameOptionsConfig.disable())
                 )
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.POST, "/api/v1/*").hasAnyRole("USER","ADMIN")
-                        .requestMatchers(HttpMethod.PATCH,"/api/v1/*").hasAnyRole("USER","ADMIN")
-                        .anyRequest().permitAll())
-//                .oauth2Login(configurer -> configurer.successHandler(oauthSuccessHandler)
-//                        .failureHandler(oauthFailureHandler)
-//                )
+                        .requestMatchers(HttpMethod.POST, "/api/v1/*").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/*").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/**").permitAll()  // GET 요청은 인증 없이 접근 가능
+                        .anyRequest().authenticated()) // 나머지 요청은 인증 필요
+                .oauth2Login(configurer -> configurer.successHandler(oauthSuccessHandler)
+                        .failureHandler(oauthFailureHandler)
+                )
                 .exceptionHandling(configurer -> configurer.authenticationEntryPoint(new EntryPointUnauthorizedHandler(new ObjectMapper())));
 
-
         return http.build();
+
 
     }
 }
