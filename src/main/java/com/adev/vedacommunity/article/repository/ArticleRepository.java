96358743fc94660074ee0af64d3e2dd1f420ae2f
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-09 21:56:43 +0900

    Feat : 게시글 가져오는 쿼리 추가

--- src/main/java/com/adev/vedacommunity/article/repository/ArticleRepository.java
+++ src/main/java/com/adev/vedacommunity/article/repository/ArticleRepository.java
@@ -2,10 +2,16 @@ package com.adev.vedacommunity.article.repository;
 
 import com.adev.vedacommunity.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
+    @Query("SELECT a FROM Article a JOIN FETCH a.author auth LEFT JOIN FETCH auth.company WHERE a.id = :articleId")
+    Optional<Article> findArticleWithAuthorById(@Param("articleId") Long articleId);
 
 }
