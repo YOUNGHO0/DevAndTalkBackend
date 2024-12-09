fc4032d628823d6dca37343f0b5025dd335a4f8f
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-09 22:19:53 +0900

    Feat : 테스트용도 추가

--- src/test/java/com/adev/vedacommunity/article/controller/ArticleControllerTest.java
+++ src/test/java/com/adev/vedacommunity/article/controller/ArticleControllerTest.java
@@ -1,12 +1,19 @@
 package com.adev.vedacommunity.article.controller;
 
-import com.adev.vedacommunity.article.dto.ArticleCreateDto;
+import com.adev.vedacommunity.article.dto.request.ArticleCreateDto;
+import com.adev.vedacommunity.article.dto.request.ArticleDeleteDto;
+import com.adev.vedacommunity.article.dto.request.ArticleReadDto;
+import com.adev.vedacommunity.article.dto.request.ArticleUpdateDto;
+import com.adev.vedacommunity.article.entity.Article;
+import com.adev.vedacommunity.article.mapper.ArticleMapper;
+import com.adev.vedacommunity.article.repository.ArticleRepository;
 import com.adev.vedacommunity.user.entity.CommunityUser;
 import com.adev.vedacommunity.user.repository.UserRepository;
 import com.adev.vedacommunity.user.service.UserService;
 import com.google.gson.Gson;
+import jakarta.persistence.EntityManager;
 import jakarta.servlet.http.HttpSession;
-import org.assertj.core.api.Assertions;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,7 +37,6 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -40,9 +46,7 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(RestDocumentationExtension.class)
@@ -50,6 +54,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @ActiveProfiles("local")
 @AutoConfigureRestDocs
+@Transactional
 class ArticleControllerTest {
 
 
@@ -60,10 +65,14 @@ class ArticleControllerTest {
 
     @Autowired
     private UserRepository userRepository;
-
+    @Autowired
+    private ArticleMapper articleMapper;
     @Autowired
     private UserService userService;
-
+    @Autowired
+    private ArticleRepository articleRepository;
+    @Autowired
+    private EntityManager em;
 
     private RequestFieldsSnippet requestFieldsSnippet =  requestFields(
             fieldWithPath("title").type(JsonFieldType.STRING).description("게시판 제목"),
@@ -100,10 +109,17 @@ class ArticleControllerTest {
         return mockHttpSession;
     }
 
+    CommunityUser getTestUser(){
+
+        return userRepository.findByEmail("test@gmail.com")
+                .orElseThrow(() -> new RuntimeException("No User"));
+
+    }
+
     @Test
     @DisplayName("정상적인 요청을 하면 글이 작성된다")
     void createArticle() throws Exception {
-
+        getSavedUserSession();
 
         ArticleCreateDto dto  = new ArticleCreateDto("test title", "test content");
         String json = new Gson().toJson(dto);
@@ -113,19 +129,93 @@ class ArticleControllerTest {
                 .session(getSavedUserSession())
                 .content(json)
         ).andExpect(status().isOk())
-        .andDo(print());
+        .andDo(document("articleCreate/success",
+                            operationRequestPreprocessor,
+                            operationResponsePreprocessor,
+                            requestFieldsSnippet));
 
     }
 
     @Test
-    void getArticle() {
+    @DisplayName("올바른 요청은 게시글을 반환한다.")
+    void getArticle() throws Exception {
+
+        getSavedUserSession();
+
+        RequestFieldsSnippet snippet =  requestFields(
+                fieldWithPath("articleId").type(JsonFieldType.NUMBER).description("게시판 번호")
+        );
+
+        CommunityUser user = getTestUser();
+        ArticleCreateDto dto = new ArticleCreateDto("testtitle", "test content");
+        Article saved = articleRepository.save(articleMapper.toArticle(dto, user));
+
+        ArticleReadDto readDto = new ArticleReadDto(saved.getId());
+        String json = new Gson().toJson(readDto);
+        this.mockMvc.perform(get("/api/v1/article")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json)
+                )
+                .andExpect(status().isOk())
+                .andDo(document("articleRead/success",
+                        operationRequestPreprocessor,
+                        operationResponsePreprocessor
+                ));
     }
 
     @Test
-    void updateArticle() {
+    @DisplayName("올바른 요청은 수정된다")
+    void updateArticle() throws Exception {
+        MockHttpSession savedUserSession = getSavedUserSession();
+        CommunityUser testUser = getTestUser();
+        ArticleCreateDto dto = new ArticleCreateDto("testtitle", "test content");
+        Article saved = articleRepository.save(articleMapper.toArticle(dto,testUser));
+
+        ArticleUpdateDto updateDto = new ArticleUpdateDto(saved.getId(),"updated","updated");
+        String json = new Gson().toJson(updateDto);
+
+        this.mockMvc.perform(patch("/api/v1/article")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(json)
+                        .session(savedUserSession)
+                )
+                .andExpect(status().isOk())
+                .andDo(document("articleUpdate/success",
+                        operationRequestPreprocessor,
+                        operationResponsePreprocessor
+                ));
+
+
+
+
     }
 
     @Test
-    void deleteArticle() {
+    @DisplayName("올바른 삭제요청이 들어오면 삭제된다")
+    void deleteArticle() throws Exception {
+        MockHttpSession savedUserSession = getSavedUserSession();
+        CommunityUser testUser = getTestUser();
+        ArticleCreateDto dto = new ArticleCreateDto("testtitle", "test content");
+        Article saved = articleRepository.save(articleMapper.toArticle(dto,testUser));
+
+        ArticleDeleteDto deleteDto = new ArticleDeleteDto(saved.getId());
+        String json = new Gson().toJson(deleteDto);
+
+        this.mockMvc.perform(delete("/api/v1/article")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(json)
+                        .session(savedUserSession)
+                )
+                .andExpect(status().isOk())
+                .andDo(document("articleDelete/success",
+                        operationRequestPreprocessor,
+                        operationResponsePreprocessor
+                ));
+
+
+
     }
 }
