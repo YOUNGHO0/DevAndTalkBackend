de4ddaa19f6ffba030191b0056680bbb9a5ea1cf
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-10 16:48:46 +0900

    Feat : 사용자 정보를 가져오는 Page정보 추가

--- src/test/java/com/adev/vedacommunity/BaseRestDocsTest.java
+++ src/test/java/com/adev/vedacommunity/BaseRestDocsTest.java
@@ -10,6 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
@@ -20,7 +23,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureMockMvc
 @SpringBootTest
-@ActiveProfiles("local")
 @AutoConfigureRestDocs
 @Transactional
 public class BaseRestDocsTest {
@@ -30,6 +32,9 @@ public class BaseRestDocsTest {
 
     protected MockMvc mockMvc;
 
+
+    protected OperationRequestPreprocessor operationRequestPreprocessor = Preprocessors.preprocessRequest(Preprocessors.prettyPrint());
+    protected OperationResponsePreprocessor operationResponsePreprocessor =  Preprocessors.preprocessResponse(Preprocessors.prettyPrint());
     @BeforeEach
     public void setUp(RestDocumentationContextProvider restDocumentation) {
         this.mockMvc = webAppContextSetup(context)
