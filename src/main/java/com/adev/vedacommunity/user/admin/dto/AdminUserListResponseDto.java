7f5ff23e30880cc0cca28e59f2100ede7addd506
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-10 11:27:59 +0900

    Feat : 관리자가 확인 가능한 AdminUserListResponseDto 추가

---
+++ src/main/java/com/adev/vedacommunity/user/admin/dto/AdminUserListResponseDto.java
@@ -0,0 +1,17 @@
+package com.adev.vedacommunity.user.admin.dto;
+
+import com.adev.vedacommunity.user.dto.response.CompanyReadResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class AdminUserListResponseDto {
+
+    private long id;
+    private String nickname;
+    String email;
+    long vedaOrder;
+    CompanyReadResponseDto company;
+}
