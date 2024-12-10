3b8fc0e2dbc2fee10fffb93136501c635eec4fd5
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-10 16:53:02 +0900

    Feat : ArticleRepository 쿼리추가

--- src/main/java/com/adev/vedacommunity/article/repository/ArticleRepository.java
+++ src/main/java/com/adev/vedacommunity/article/repository/ArticleRepository.java
@@ -1,6 +1,8 @@
 package com.adev.vedacommunity.article.repository;
 
 import com.adev.vedacommunity.article.entity.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,5 +15,8 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     @Query("SELECT a FROM Article a JOIN FETCH a.author auth LEFT JOIN FETCH auth.company WHERE a.id = :articleId")
     Optional<Article> findArticleWithAuthorById(@Param("articleId") Long articleId);
+
+    @Query("SELECT a FROM Article a JOIN FETCH a.author auth LEFT JOIN FETCH auth.company")
+    Page<Article> getArticleList(Pageable pageable);
 
 }
