f16537a54eaf9edc6ba426f07e302031ea165add
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-10 16:52:28 +0900

    Feat : 변환하는 MapperFunction추가

--- src/main/java/com/adev/vedacommunity/article/mapper/ArticleMapper.java
+++ src/main/java/com/adev/vedacommunity/article/mapper/ArticleMapper.java
@@ -7,10 +7,14 @@ import com.adev.vedacommunity.article.entity.Article;
 import com.adev.vedacommunity.user.dto.response.CommunityUserReadResponseDto;
 import com.adev.vedacommunity.user.entity.CommunityUser;
 import org.mapstruct.Mapper;
-
+import org.springframework.data.domain.Page;
 @Mapper(componentModel = "spring")
 public interface ArticleMapper {
 
     Article toArticle(ArticleCreateDto dto, CommunityUser author);
     ArticleReadResponseDto toArticleReadDto(Article article);
+    // Page<Article>을 Page<ArticleReadResponseDto>로 변환
+    default Page<ArticleReadResponseDto> toReadPageDto(Page<Article> articles) {
+        return articles.map(this::toArticleReadDto);
+    }
 }
--- src/main/java/com/adev/vedacommunity/mapper/AdminMapper.java
+++ src/main/java/com/adev/vedacommunity/mapper/AdminMapper.java
@@ -1,11 +1,18 @@
 package com.adev.vedacommunity.mapper;
 
-import com.adev.vedacommunity.user.admin.dto.AdminUserListResponseDto;
+import com.adev.vedacommunity.admin.dto.AdminUserListResponseDto;
 import com.adev.vedacommunity.user.entity.CommunityUser;
 import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+
 
 @Mapper(componentModel = "spring")
 public interface AdminMapper {
 
     AdminUserListResponseDto toAdminUserDto(CommunityUser user);
+
+    default Page<AdminUserListResponseDto> toAdminPageReadDto(Page<CommunityUser> communityUsers){
+
+        return communityUsers.map(this::toAdminUserDto);
+    }
 }
