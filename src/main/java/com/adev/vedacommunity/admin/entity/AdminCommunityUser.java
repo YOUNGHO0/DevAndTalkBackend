7dbe580b56e69da13c83b1cdcb1a908ab21163ff
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-10 16:52:08 +0900

    Fix : AdminCommunityUser 생성자와 권한 변경

--- src/main/java/com/adev/vedacommunity/admin/entity/AdminCommunityUser.java
+++ src/main/java/com/adev/vedacommunity/admin/entity/AdminCommunityUser.java
@@ -1,25 +1,28 @@
-package com.adev.vedacommunity.user.admin.entity;
+package com.adev.vedacommunity.admin.entity;
 
 import com.adev.vedacommunity.user.entity.CommunityUser;
 import com.adev.vedacommunity.user.entity.Company;
+import jakarta.persistence.Entity;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+@Entity
 public class AdminCommunityUser extends CommunityUser {
+
+    protected AdminCommunityUser() {
+
+    }
 
     private String adminUserName;
 
-    public AdminCommunityUser(String nickname, String email, Company company, String adminUserName) {
-        super(nickname, email, company);
+    public AdminCommunityUser(String email, String nickName, String adminUserName) {
+        super(email, nickName);
+        this.adminUserName = adminUserName;
         authorities.add((GrantedAuthority) () -> "ROLE_ADMIN");
         this.adminUserName = adminUserName;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
 
-        return authorities;
-    }
 }
