b00f3bea5d287ffa5fde07e2c509bbaa0b441bb0
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-11 15:11:48 +0900

    Feat : equals hashcode 재정의

--- src/main/java/com/adev/vedacommunity/article/entity/ActiveArticle.java
+++ src/main/java/com/adev/vedacommunity/article/entity/ActiveArticle.java
@@ -5,11 +5,27 @@ import com.adev.vedacommunity.user.entity.CommunityUserView;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.hibernate.annotations.Immutable;
+import java.util.Objects;
 
 @Entity
 @Immutable  // 뷰는 읽기 전용이므로 이 어노테이션을 추가
 @Getter // 생성한 뷰 이름을 테이블처럼 사용
 public class ActiveArticle {
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true; // 같은 객체인지 확인
+        if (o == null || getClass() != o.getClass()) return false; // 클래스 타입 확인
+        ActiveArticle that = (ActiveArticle) o;
+        return id == that.id; // id가 동일한지 확인
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id); // id를 기반으로 hashCode 생성
+    }
+
 
     protected ActiveArticle(){};
     @Id
@@ -21,5 +37,9 @@ public class ActiveArticle {
 
     @ManyToOne
     CommunityUserView author;
+
+
+
+
 
 }
--- src/main/java/com/adev/vedacommunity/user/entity/CommunityUserView.java
+++ src/main/java/com/adev/vedacommunity/user/entity/CommunityUserView.java
@@ -5,19 +5,35 @@ import lombok.Getter;
 import org.hibernate.annotations.Immutable;
 import org.springframework.security.core.GrantedAuthority;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 @Entity
 @Immutable  // 뷰는 읽기 전용이므로 이 어노테이션을 추가
 @Getter // 생성한 뷰 이름을 테이블처럼 사용
 public class CommunityUserView {
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true; // 같은 객체인지 확인
+        if (o == null || getClass() != o.getClass()) return false; // 클래스 타입 확인
+        CommunityUserView that = (CommunityUserView) o;
+        return id == that.id; // id가 동일한지 확인
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id); // id를 기반으로 hashCode 생성
+    }
+
     protected CommunityUserView() {
+        authorities = new ArrayList<>();
+        authorities.add((GrantedAuthority) () -> "ROLE_USER");
     }
 
     @Transient
-    protected Collection<GrantedAuthority> authorities;
-
+    Collection<GrantedAuthority> authorities;
     @Id
     private long id;
     private String email;
