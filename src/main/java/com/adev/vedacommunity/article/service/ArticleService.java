417cb58bc01e9fd3cdae43ad372c43b2f7afed45
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-11 14:03:08 +0900

    Fix : CommunityUserView 사용하게 설정

--- src/main/java/com/adev/vedacommunity/article/service/ArticleService.java
+++ src/main/java/com/adev/vedacommunity/article/service/ArticleService.java
@@ -31,11 +31,11 @@ public class ArticleService {
     }
 
 
-    public void update(long id,String title, String content, CommunityUser user) {
+    public void update(long id,String title, String content, CommunityUserView user) {
         articleRepository.findById(id).ifPresent((article)->{ article.update(title, content, user);});
     }
 
-    public void delete(long id, CommunityUser user){
+    public void delete(long id, CommunityUserView user){
         articleRepository.findById(id).ifPresent((article)->{ if(article.canDelete(user)) articleRepository.delete(article);});
     }
 }
