368a7fe1cc5b2d64ea51441ed63b4c0e0d023a0e
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-14 00:34:14 +0900

    Fix : ActiveArticle 사용하게 수정

--- src/main/java/com/adev/vedacommunity/anonarticle/mapper/AnonArticleMapper.java
+++ src/main/java/com/adev/vedacommunity/anonarticle/mapper/AnonArticleMapper.java
@@ -1,19 +1,17 @@
 package com.adev.vedacommunity.anonarticle.mapper;
 
 import com.adev.vedacommunity.anonarticle.dto.request.AnonArticleCreateRequestDto;
-import com.adev.vedacommunity.anonarticle.dto.request.AnonArticleReadRequestDto;
-import com.adev.vedacommunity.anonarticle.dto.request.AnonArticleUpdateRequestDto;
 import com.adev.vedacommunity.anonarticle.dto.response.AnonArticleReadResponseDto;
+import com.adev.vedacommunity.anonarticle.entity.ActiveAnonArticle;
 import com.adev.vedacommunity.anonarticle.entity.AnonArticle;
-import com.adev.vedacommunity.user.entity.CommunityUser;
 import com.adev.vedacommunity.user.entity.CommunityUserView;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface AnonArticleMapper {
 
-    AnonArticleReadResponseDto toReadDto(AnonArticle anonArticle);
+    AnonArticleReadResponseDto toReadDto(ActiveAnonArticle anonArticle);
     AnonArticle toAnonArticle(AnonArticleCreateRequestDto dto, CommunityUserView author);
-    AnonArticle toAnonArticle(AnonArticleUpdateRequestDto dto, CommunityUserView author);
+
 
 }
