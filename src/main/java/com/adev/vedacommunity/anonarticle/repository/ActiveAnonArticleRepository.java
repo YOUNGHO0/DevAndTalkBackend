164da51a4b61fc059e9a72e30bf5f5adc72b1c66
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-13 20:10:15 +0900

    Feat : Repository 생성

---
+++ src/main/java/com/adev/vedacommunity/anonarticle/repository/ActiveAnonArticleRepository.java
@@ -0,0 +1,8 @@
+package com.adev.vedacommunity.anonarticle.repository;
+
+import com.adev.vedacommunity.anonarticle.entity.ActiveAnonArticle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActiveAnonArticleRepository extends JpaRepository<ActiveAnonArticle, Long> {
+
+}
---
+++ src/main/java/com/adev/vedacommunity/anonarticle/repository/AnonArticleRepository.java
@@ -0,0 +1,7 @@
+package com.adev.vedacommunity.anonarticle.repository;
+
+import com.adev.vedacommunity.anonarticle.entity.AnonArticle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnonArticleRepository extends JpaRepository<AnonArticle, Long> {
+}
