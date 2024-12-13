68cba7bd3c869497f4469dcb37ddf67f91e63bab
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-13 18:48:58 +0900

    Fix : AdminController 추가 * 관리자 권한 변경 * 게시글 강제 삭제 구현

--- src/main/java/com/adev/vedacommunity/admin/controller/AdminController.java
+++ src/main/java/com/adev/vedacommunity/admin/controller/AdminController.java
@@ -1,35 +1,53 @@
 package com.adev.vedacommunity.admin.controller;
 
+import com.adev.vedacommunity.admin.dto.AdminUserChangeRequestDto;
 import com.adev.vedacommunity.admin.dto.AdminUserListResponseDto;
+import com.adev.vedacommunity.admin.dto.AdminArticleDeleteRequestDto;
+import com.adev.vedacommunity.admin.service.AdminService;
+import com.adev.vedacommunity.article.entity.Article;
+import com.adev.vedacommunity.article.repository.ArticleRepository;
 import com.adev.vedacommunity.mapper.AdminMapper;
 import com.adev.vedacommunity.user.entity.CommunityUser;
 import com.adev.vedacommunity.user.entity.CommunityUserView;
-import com.adev.vedacommunity.user.repository.CommunityUserRepository;
 import com.adev.vedacommunity.user.repository.CommunityUserViewRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/admin")
 @RestController
 @RequiredArgsConstructor
 public class AdminController {
 
     private final CommunityUserViewRepository communityUserRepository;
     private final AdminMapper adminMapper;
+    private final AdminService adminService;
+    private final ArticleRepository articleRepository;
     @GetMapping("")
     public ResponseEntity findAllUser(@AuthenticationPrincipal CommunityUser user, Pageable pageable) {
 
         Page<CommunityUserView> all = communityUserRepository.findAll(pageable);
         Page<AdminUserListResponseDto> adminPageDto = adminMapper.toAdminPageReadDto(all);
 
-
         return ResponseEntity.ok().body(adminPageDto);
     }
+    @PostMapping("/upgrade")
+    public ResponseEntity changeUsertoAdmin(@AuthenticationPrincipal CommunityUserView user , @RequestBody AdminUserChangeRequestDto dto){
+
+        adminService.changeUsertoAdmin(dto.getUserId(), dto.getAdminUserName());
+        return ResponseEntity.ok().build();
+    }
+
+    @Transactional
+    @DeleteMapping("/article")
+    public ResponseEntity deleteArticleForce(@RequestBody AdminArticleDeleteRequestDto dto,  @AuthenticationPrincipal CommunityUserView user ){
+        articleRepository.findById(dto.getArticleId()).ifPresent(article -> article.delete());
+        return ResponseEntity.ok().build();
+    }
+
 
 }
