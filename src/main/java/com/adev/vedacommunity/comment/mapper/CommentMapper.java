4eea0b7563fcbdd1459e22b92f5eba8e5eb2496f
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-14 19:31:18 +0900

    Feat: CommentMapper 생성

---
+++ src/main/java/com/adev/vedacommunity/comment/mapper/CommentMapper.java
@@ -0,0 +1,22 @@
+package com.adev.vedacommunity.comment.mapper;
+
+import com.adev.vedacommunity.article.entity.ActiveArticle;
+import com.adev.vedacommunity.comment.dto.read.CommentReadResponseDto;
+import com.adev.vedacommunity.comment.dto.request.CommentCreateRequestDto;
+import com.adev.vedacommunity.comment.entity.ActiveComment;
+import com.adev.vedacommunity.comment.entity.Comment;
+import com.adev.vedacommunity.user.entity.CommunityUserView;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+    @Mapping(target = "articleId", source = "comment.article.id")
+    CommentReadResponseDto toDto(ActiveComment comment);
+    default Comment toComment(String commentContent, CommunityUserView commentAuthor, ActiveArticle article){
+        return new Comment(commentContent,commentAuthor,article);
+    }
+
+
+}
