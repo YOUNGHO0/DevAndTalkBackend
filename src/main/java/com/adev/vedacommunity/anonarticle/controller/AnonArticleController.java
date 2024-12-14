f13a068bff6a3fc2a1fcec909633c9f8b785611e
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-14 11:33:32 +0900

    Fix : requestMapping 추가

--- src/main/java/com/adev/vedacommunity/anonarticle/controller/AnonArticleController.java
+++ src/main/java/com/adev/vedacommunity/anonarticle/controller/AnonArticleController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/anonArticle")
 public class AnonArticleController {
 
     private final AnonArticleService anonArticleService;
