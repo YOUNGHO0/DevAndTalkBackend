3926fc1127d021275285dafeabff0fe69bb65f03
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-15 13:10:11 +0900

    Feat: 자식 댓글 작성 메서드 추가

--- src/main/java/com/adev/vedacommunity/comment/controller/CommentController.java
+++ src/main/java/com/adev/vedacommunity/comment/controller/CommentController.java
@@ -3,13 +3,12 @@ package com.adev.vedacommunity.comment.controller;
 import com.adev.vedacommunity.article.entity.ActiveArticle;
 import com.adev.vedacommunity.article.service.ArticleService;
 import com.adev.vedacommunity.comment.dto.read.CommentReadResponseDto;
-import com.adev.vedacommunity.comment.dto.request.CommentCreateRequestDto;
-import com.adev.vedacommunity.comment.dto.request.CommentDeleteRequestDto;
-import com.adev.vedacommunity.comment.dto.request.CommentReadRequestDto;
-import com.adev.vedacommunity.comment.dto.request.CommentUpdateRequestDto;
+import com.adev.vedacommunity.comment.dto.request.*;
 import com.adev.vedacommunity.comment.entity.ActiveComment;
 import com.adev.vedacommunity.comment.entity.Comment;
 import com.adev.vedacommunity.comment.mapper.CommentMapper;
+import com.adev.vedacommunity.comment.repository.ActiveCommentRepository;
+import com.adev.vedacommunity.comment.repository.CommentRepository;
 import com.adev.vedacommunity.comment.service.CommentService;
 import com.adev.vedacommunity.user.entity.CommunityUserView;
 import lombok.RequiredArgsConstructor;
@@ -25,12 +24,22 @@ public class CommentController {
     private final CommentService commentService;
     private final CommentMapper commentMapper;
     private final ArticleService articleService;
+    private final ActiveCommentRepository activeCommentRepository;
     @PostMapping
     public ResponseEntity creatComment(@RequestBody CommentCreateRequestDto dto, @AuthenticationPrincipal CommunityUserView user){
 
         ActiveArticle article = articleService.read(dto.getArticleId()).orElseThrow(() -> new RuntimeException("No article found"));
         Comment comment = commentMapper.toComment("commentContent", user, article);
         commentService.createComment(comment);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/child")
+    public ResponseEntity createChildComment(@RequestBody ChildCommentCreateRequestDto dto, @AuthenticationPrincipal CommunityUserView user){
+
+        ActiveComment parentComment = activeCommentRepository.findById(dto.getParentId()).orElseThrow(() -> new RuntimeException("No comment found"));
+        Comment childComment = commentMapper.toChildComment(dto, parentComment, user, parentComment.getArticle());
+        commentService.createComment(childComment);
         return ResponseEntity.ok().build();
     }
 
