2b0a1585417c6e5c362bf9c7091828f59b441f46
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-15 13:09:51 +0900

    Feat: 부모 댓글 ActiveComment로 변경

--- src/main/java/com/adev/vedacommunity/comment/entity/Comment.java
+++ src/main/java/com/adev/vedacommunity/comment/entity/Comment.java
@@ -1,54 +1,67 @@
-package com.adev.vedacommunity.comment.entity;
+    package com.adev.vedacommunity.comment.entity;
 
 
-import com.adev.vedacommunity.article.entity.ActiveArticle;
-import com.adev.vedacommunity.logging.BaseTimeEntity;
-import com.adev.vedacommunity.user.entity.CommunityUserView;
-import jakarta.persistence.*;
+    import com.adev.vedacommunity.article.entity.ActiveArticle;
+    import com.adev.vedacommunity.logging.BaseTimeEntity;
+    import com.adev.vedacommunity.user.entity.CommunityUserView;
+    import jakarta.persistence.*;
+    import lombok.Setter;
 
-@Entity
-public class Comment extends BaseTimeEntity {
+    import java.util.ArrayList;
+    import java.util.List;
 
-    public Comment() {
+    @Entity
+    public class Comment extends BaseTimeEntity {
 
-    }
+        public Comment() {}
 
-    public Comment(String commentContent, CommunityUserView commentAuthor, ActiveArticle article ){
-        this.commentContent = commentContent;
-        this.commentAuthor = commentAuthor;
-        this.article = article;
-    }
+        public Comment(String commentContent, CommunityUserView commentAuthor, ActiveArticle article ){
+            this.commentContent = commentContent;
+            this.commentAuthor = commentAuthor;
+            this.article = article;
+        }
+        public Comment(String commentContent, CommunityUserView commentAuthor, ActiveArticle article, ActiveComment parentComment ){
+            this.commentContent = commentContent;
+            this.commentAuthor = commentAuthor;
+            this.article = article;
+            this.parentComment = parentComment;
+        }
 
-    public boolean canCreate(){
+        @OneToMany(mappedBy = "parentComment",fetch = FetchType.LAZY)
+        List<Comment> commentList = new ArrayList<>();
 
-        return true;
-    }
-    public boolean canUpdate(CommunityUserView requestUser){
-        if(this.commentAuthor.equals(requestUser)){
+
+        public boolean canCreate(){
+
             return true;
         }
-        return false;
-    }
-    public boolean canDelete(CommunityUserView requestUser){
-        return true;
-    }
-    public void delete(){
-        this.isDeleted = true;
-    }
-    public void update (String commentContent){
-        this.commentContent = commentContent;
-    }
+        public boolean canUpdate(CommunityUserView requestUser){
+            if(this.commentAuthor.equals(requestUser)){
+                return true;
+            }
+            return false;
+        }
+        public boolean canDelete(CommunityUserView requestUser){
+            if(!this.commentAuthor.equals(requestUser)) throw new RuntimeException("Author not match");
+            return true;
+        }
+        public void delete(){
+            this.isDeleted = true;
+        }
+        public void update (String commentContent){
+            this.commentContent = commentContent;
+        }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
-    String commentContent;
-    boolean isDeleted = false;
-    @ManyToOne(fetch = FetchType.LAZY)
-    CommunityUserView commentAuthor;
-    @ManyToOne(fetch = FetchType.LAZY)
-    ActiveArticle article;
-    @ManyToOne(fetch = FetchType.LAZY)
-    Comment parentComment;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        long id;
+        String commentContent;
+        boolean isDeleted = false;
+        @ManyToOne(fetch = FetchType.LAZY)
+        CommunityUserView commentAuthor;
+        @ManyToOne(fetch = FetchType.LAZY)
+        ActiveArticle article;
+        @ManyToOne(fetch = FetchType.LAZY)
+        ActiveComment parentComment;
 
-}
+    }
