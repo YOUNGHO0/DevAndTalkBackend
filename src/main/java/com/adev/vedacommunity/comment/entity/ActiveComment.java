2b998691806d680db2efb8b6eff765aa5c36b5fb
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-15 13:43:12 +0900

    Fix: ActiveComment equals 변경 * 프록시 때문에 hibernate.getclass 사용

--- src/main/java/com/adev/vedacommunity/comment/entity/ActiveComment.java
+++ src/main/java/com/adev/vedacommunity/comment/entity/ActiveComment.java
@@ -1,18 +1,39 @@
 package com.adev.vedacommunity.comment.entity;
 
 import com.adev.vedacommunity.article.entity.ActiveArticle;
+import com.adev.vedacommunity.article.entity.Article;
 import com.adev.vedacommunity.user.entity.CommunityUserView;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Hibernate;
 import org.hibernate.annotations.Immutable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Immutable
 @Entity
 @Getter
 public class ActiveComment {
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        ActiveComment that = (ActiveComment) o;
+        return Objects.equals(id, that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id); // id 기반으로 hashCode 생성
+    }
 
     protected ActiveComment(){};
     @Id
