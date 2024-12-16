a7b558d00a9d32b8cb39402b75d5d80e37b2e081
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-16 19:17:15 +0900

    Fix: 생성자 변경

--- src/main/java/com/adev/vedacommunity/comment/dto/request/CommentUpdateRequestDto.java
+++ src/main/java/com/adev/vedacommunity/comment/dto/request/CommentUpdateRequestDto.java
@@ -1,10 +1,11 @@
 package com.adev.vedacommunity.comment.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class CommentUpdateRequestDto {
 
     long commentId;
