4ed8f2e82403381b0690efad6129e4abd6184579
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-16 19:16:21 +0900

    Fix: 테스트용 allArgsConstructor 추가

--- src/main/java/com/adev/vedacommunity/comment/dto/request/ChildCommentCreateRequestDto.java
+++ src/main/java/com/adev/vedacommunity/comment/dto/request/ChildCommentCreateRequestDto.java
@@ -1,7 +1,8 @@
 package com.adev.vedacommunity.comment.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-
+@AllArgsConstructor
 @Getter
 public class ChildCommentCreateRequestDto {
     long parentId;
