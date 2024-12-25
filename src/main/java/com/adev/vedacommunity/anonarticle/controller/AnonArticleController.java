65e3a2819fe6b00a48f2c186588c815335681680
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-25 22:41:43 +0900

    Feat : 익명게시판 리스트 반환 컨트롤러 추가

--- src/main/java/com/adev/vedacommunity/anonarticle/controller/AnonArticleController.java
+++ src/main/java/com/adev/vedacommunity/anonarticle/controller/AnonArticleController.java
@@ -8,9 +8,13 @@ import com.adev.vedacommunity.anonarticle.dto.response.AnonArticleReadResponseDt
 import com.adev.vedacommunity.anonarticle.entity.ActiveAnonArticle;
 import com.adev.vedacommunity.anonarticle.entity.AnonArticle;
 import com.adev.vedacommunity.anonarticle.mapper.AnonArticleMapper;
+import com.adev.vedacommunity.anonarticle.repository.ActiveAnonArticleRepository;
+import com.adev.vedacommunity.anonarticle.repository.AnonArticleRepository;
 import com.adev.vedacommunity.anonarticle.service.AnonArticleService;
 import com.adev.vedacommunity.user.entity.CommunityUserView;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +26,9 @@ public class AnonArticleController {
 
     private final AnonArticleService anonArticleService;
     private final AnonArticleMapper anonArticleMapper;
+    private final AnonArticleRepository anonArticleRepository;
+    private final ActiveAnonArticleRepository activeAnonArticleRepository;
+
     @PostMapping
     public ResponseEntity createAnonArticle(@RequestBody AnonArticleCreateRequestDto requestDto , @AuthenticationPrincipal CommunityUserView communityUser){
         AnonArticle anonArticle = anonArticleMapper.toAnonArticle(requestDto, communityUser);
@@ -48,4 +55,17 @@ public class AnonArticleController {
         anonArticleService.delete(deleteRequestDto.getAnonArticleId(),user);
         return ResponseEntity.ok().build();
     };
+
+    @GetMapping("/{id}")
+    public ResponseEntity getArticle(@PathVariable long id) {
+        ActiveAnonArticle anonArticle = anonArticleService.read(id); // id를 사용하여 데이터 조회
+
+        return ResponseEntity.ok(anonArticleMapper.toReadDto(anonArticle));
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity getArticle(Pageable pageable){
+        Page<ActiveAnonArticle> list = activeAnonArticleRepository.getArticleList(pageable);
+        return ResponseEntity.ok().body(anonArticleMapper.toAnonArticleReadResponseDto(list));
+    }
 }
