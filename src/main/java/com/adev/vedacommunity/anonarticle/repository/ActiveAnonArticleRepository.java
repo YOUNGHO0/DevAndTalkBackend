ae030782ac199c18d63ed3c43396f3e2f15bc179
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-25 22:41:21 +0900

    Feat : AnonArticle가져오는 쿼리 추가

--- src/main/java/com/adev/vedacommunity/anonarticle/repository/ActiveAnonArticleRepository.java
+++ src/main/java/com/adev/vedacommunity/anonarticle/repository/ActiveAnonArticleRepository.java
@@ -1,11 +1,17 @@
 package com.adev.vedacommunity.anonarticle.repository;
 
 import com.adev.vedacommunity.anonarticle.entity.ActiveAnonArticle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ActiveAnonArticleRepository extends JpaRepository<ActiveAnonArticle, Long> {
 
     Optional<ActiveAnonArticle> findById(Long articleId);
+
+    @Query("SELECT a FROM ActiveAnonArticle a")
+    Page<ActiveAnonArticle> getArticleList(Pageable pageable);
 }
