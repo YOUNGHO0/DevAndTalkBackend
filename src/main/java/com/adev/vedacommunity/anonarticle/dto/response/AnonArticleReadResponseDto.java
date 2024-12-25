32fdbe5c26a14da7e7b7942cedab5fee285ddc9f
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-25 22:42:48 +0900

    Feat : 반환값에 id 작성시간 추가

--- src/main/java/com/adev/vedacommunity/anonarticle/dto/response/AnonArticleReadResponseDto.java
+++ src/main/java/com/adev/vedacommunity/anonarticle/dto/response/AnonArticleReadResponseDto.java
@@ -3,12 +3,16 @@ package com.adev.vedacommunity.anonarticle.dto.response;
 import com.adev.vedacommunity.user.dto.response.CommunityUserReadResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
 
 
 @AllArgsConstructor
 @Getter
 public class AnonArticleReadResponseDto {
-
+    long id;
+    LocalDateTime createdDate;
     String title;
     String content;
     long viewCount;
