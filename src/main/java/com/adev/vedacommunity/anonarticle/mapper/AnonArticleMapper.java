6fc2d1ef02aa559635ddb00fab944c34cf34d0af
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-25 22:41:53 +0900

    Feat : 리스트 매퍼 추가

--- src/main/java/com/adev/vedacommunity/anonarticle/mapper/AnonArticleMapper.java
+++ src/main/java/com/adev/vedacommunity/anonarticle/mapper/AnonArticleMapper.java
@@ -4,14 +4,18 @@ import com.adev.vedacommunity.anonarticle.dto.request.AnonArticleCreateRequestDt
 import com.adev.vedacommunity.anonarticle.dto.response.AnonArticleReadResponseDto;
 import com.adev.vedacommunity.anonarticle.entity.ActiveAnonArticle;
 import com.adev.vedacommunity.anonarticle.entity.AnonArticle;
+import com.adev.vedacommunity.article.entity.ActiveArticle;
 import com.adev.vedacommunity.user.entity.CommunityUserView;
 import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
 
 @Mapper(componentModel = "spring")
 public interface AnonArticleMapper {
 
     AnonArticleReadResponseDto toReadDto(ActiveAnonArticle anonArticle);
     AnonArticle toAnonArticle(AnonArticleCreateRequestDto dto, CommunityUserView author);
-
+    default Page<AnonArticleReadResponseDto> toAnonArticleReadResponseDto(Page<ActiveAnonArticle> activeArticle){
+        return activeArticle.map(this::toReadDto);
+    }
 
 }
