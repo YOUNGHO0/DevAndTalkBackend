023cb6fc4b3c463b190a881715987ddfb1dab362
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-28 23:54:10 +0900

    Fix : activeProfile추가

--- src/test/java/com/adev/vedacommunity/comment/entity/CommentTest.java
+++ src/test/java/com/adev/vedacommunity/comment/entity/CommentTest.java
@@ -23,6 +23,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -30,6 +31,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("local")
 class CommentTest {
 
 
