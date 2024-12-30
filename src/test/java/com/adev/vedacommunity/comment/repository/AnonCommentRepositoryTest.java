460131265c402e590615b403e6f1d4d32dd928b7
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-30 22:21:14 +0900

    Fix : AnonCommentRepositoryTest transactional 추가

--- src/test/java/com/adev/vedacommunity/comment/repository/AnonCommentRepositoryTest.java
+++ src/test/java/com/adev/vedacommunity/comment/repository/AnonCommentRepositoryTest.java
@@ -21,10 +21,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional
 class AnonCommentRepositoryTest {
 
     @Autowired
