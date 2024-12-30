b9afa316f0bc6fe119a88b421fe67c9dd154999f
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-30 22:43:00 +0900

    Fix : @ActiveProfiles("local") 추가

--- src/test/java/com/adev/vedacommunity/comment/repository/CommentRepositoryTest.java
+++ src/test/java/com/adev/vedacommunity/comment/repository/CommentRepositoryTest.java
@@ -15,11 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
 @Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("local")
 public class CommentRepositoryTest {
 
     @Autowired
